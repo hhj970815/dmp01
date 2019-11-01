0613609563833c2f23933d0879bd07bf8f107aaf
hhj970815 <13535071943@126.com>
2019-11-01 18:52:32 +0800

    注释

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -6,7 +6,7 @@ import org.apache.spark.sql.SparkSession
 object Application {
 
   def main(args: Array[String]): Unit = {
-
+    // wode git
     //1、创建SparkSession
     val spark = SparkSession.builder()
       .master("local[3]")
